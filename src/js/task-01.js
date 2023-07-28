6897afcae1da48c578d941df0ab6b31ba2162fc0
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-07-28 15:53:17 +0300

    task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,1 @@
-const items = document.querySelectorAll(".item");
-console.log(items.length);
 
-const childrens = [...items].forEach((item) => {
-  const value = item.firstElementChild.textContent;
-  console.log(value);
-  const count = item.lastElementChild.childElementCount;
-  console.log(count);
-});
