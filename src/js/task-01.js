359a270d6f3049f9df981527008011edc641f5db
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-07-29 15:36:03 +0300

    task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,8 @@
-// const items = document.querySelectorAll(".item");
-// console.log(items.length);
-// const childrens = [...items].forEach((item) => {
-//   const value = item.firstElementChild.textContent;
-//   console.log(value);
-//   const count = item.lastElementChild.childElementCount;
-//   console.log(count);
-// });
+const items = document.querySelectorAll(".item");
+console.log(items.length);
+const childrens = [...items].forEach((item) => {
+  const value = item.firstElementChild.textContent;
+  console.log(value);
+  const count = item.lastElementChild.childElementCount;
+  console.log(count);
+});
