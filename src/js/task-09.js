6b2c06ab78282e3fd3199cb01537275e988066ab
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-07-30 18:00:45 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const elements = {
+  btn: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  bodyColor: document.querySelector("body"),
+};
+
+elements.btn.addEventListener("click", handlerChangeColor);
+
+function handlerChangeColor() {
+  const changeColor = getRandomHexColor();
+  elements.bodyColor.style.backgroundColor = changeColor;
+  elements.color.textContent = changeColor;
+}
