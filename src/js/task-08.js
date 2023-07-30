347a697657c491992ba518be84e3f9b1e17f5926
Alinemis <127789972+Alinemis@users.noreply.github.com>
2023-07-30 17:43:36 +0300

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  //   console.dir(evt.currentTarget);
+  const { email, password } = evt.currentTarget.elements;
+  const objectForm = {
+    email: email.value,
+    password: password.value,
+  };
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+
+  console.log(objectForm);
+  evt.currentTarget.reset();
+}
